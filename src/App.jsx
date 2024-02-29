a37f8563b6c48eaa74e99b0d80ad22b1e40c93e7
Ankit3060 <ankit330660@gmail.com>
2024-02-29 17:46:35 +0530

    Setup the Appwrite Auth services

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1>Blog Writing</h1>
+    </>
+  )
+}
+
+export default App
