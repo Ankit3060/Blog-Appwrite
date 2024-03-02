4ffb2ca7b8332bd3bd624e938349a2dac2c5af32
Ankit3060 <ankit330660@gmail.com>
2024-03-03 01:09:03 +0530

    Added redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -55,4 +55,4 @@ export class AuthService{
 
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
